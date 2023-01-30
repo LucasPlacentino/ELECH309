908fcf0bc224ec2a0c25becaa5991037ff266157
ticccco <23436953+LucasPlacentino@users.noreply.github.com>
2023-01-30 18:38:26 +0100

    initial files

---
+++ test.c
@@ -0,0 +1,13 @@
+/*
+ * File:   test.c
+ * Author: lucasp
+ *
+ * Created on 30 janvier 2023, 18:28
+ */
+
+
+#include "xc.h"
+
+int testing(int res) {
+    return res;
+}
---
+++ test.h
@@ -0,0 +1,9 @@
+/* 
+ * File:   test.h
+ * Author: lucasp
+ *
+ * Created on 30 janvier 2023, 18:37
+ */
+
+int testing(int res);
+
